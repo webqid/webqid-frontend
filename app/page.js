0c453a8d1cd7a6603763f753bbc38ee8f1a561aa
jcvandeweerd <jcvandeweerd@gmail.com>
2024-10-11 15:00:18 +0200

    Delete blank

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
         <div className="text-center w-full">
           <div className="text-[92px] lg:text-[192px]"><span className="text-blue-500">web</span><span className="text-pink-600">qid</span></div>
           <div className="text-xl lg:text-2xl text-blue-500">Unlocking the Power of Web3</div>
