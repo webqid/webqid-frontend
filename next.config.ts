a5a033a0e108a3ae168371a0f882534d83cde38a
jcvandeweerd <jcvandeweerd@gmail.com>
2025-10-17 09:46:35 +0200

    chore: update config header sync

--- next.config.ts
+++ next.config.ts
@@ -1,49 +1,70 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' vercel.live;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+  font-src 'self' fonts.gstatic.com;
+  img-src 'self' blob: data: placehold.co vercel.live;
+  connect-src 'self' https://rpc.ankr.com https://*.alchemy.com https://*.infura.io https://api.vercel.com https://*.githubusercontent.com;
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`
+  .replace(/\s{2,}/g, " ")
+  .trim()
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  headers: async () => ([
-    {
-      source: "/_next/static/:path*",
-      headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }],
-    },
-    {
-      source: "/fonts/:path*",
-      headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }],
-    },
-    {
-        source: '/(.*)',
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
           },
         ],
       },
-  ]),
+      {
+        source: "/fonts/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader,
+          },
+        ],
+      },
+    ]
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "placehold.co",
       },
+      {
+        protocol: "https",
+        hostname: "vercel.live",
+      },
     ],
-    formats: ["image/avif", "image/webp"]
+    formats: ["image/avif", "image/webp"],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
